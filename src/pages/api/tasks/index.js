bed606b98555017080d71d8eb133fc8f8973a478
Victor Perea <andresorjuela1995@hotmail.com>
2021-11-09 00:42:11 -0500

    conexion a base de datos y dependencia de servidor graphql

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -3,5 +3,6 @@ import { dbConnect } from "../../../utils/mongoose";
 dbConnect();
 
 export default function handler(req, res) {
-  res.status(200).json("tasks");
+  res.status(200).json("hola");
+  // MONGODB_URL=mongodb://localhost:27017/tablerodetareas
 }
