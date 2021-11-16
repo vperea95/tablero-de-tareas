78d3eac2926b8eece7da48d93fcf852c4428017a
Victor Perea <andresorjuela1995@hotmail.com>
2021-11-15 21:09:41 -0500

    cambios lebes

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,7 +1,9 @@
 import { dbConnect } from "../../../utils/mongoose";
 
 dbConnect();
+server();
 
 export default function handler(req, res) {
-  res.status(200).json("tasks");
+  res.status(200).json("hola");
+  // MONGODB_URL=mongodb://localhost:27017/tablerodetareas
 }
